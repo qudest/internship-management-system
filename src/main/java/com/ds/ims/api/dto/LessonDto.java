0725c74ebc0c71fb5af958a784bfd01317dfc7a0
Nagornyi Arseniy <nagornyi.arsenii@mail.ru>
2024-04-24 20:27:04 +0300

    LessonDto impl

--- src/main/java/com/ds/ims/api/dto/LessonDto.java
+++ src/main/java/com/ds/ims/api/dto/LessonDto.java
@@ -1,4 +1,12 @@
 package com.ds.ims.api.dto;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class LessonDto {
+    String title;
+    String description;
 }
